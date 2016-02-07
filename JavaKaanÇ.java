851d01f5e0de06e980572c83d3d7e0ddbc437ba5
APP iOS X <ardamavi2@gmail.com>
2016-02-07 19:57:06 +0200

    Create JavaKaanÇ.java

---
+++ JavaKaanÇ.java
@@ -0,0 +1,27 @@
+
+// Arda Mavi - APP iOS X
+
+int result = 0;
+        for(int i = 0; i < 5; i++){
+        
+            if (i == 3){
+            
+                result += 10;
+            
+            }else {
+            
+                result += i;
+            
+            }
+        
+        }
+        
+        System.out.println(result);
+        
+    /*
+    
+    Konsol Çıktısı :
+    
+    17
+    
+    */    
